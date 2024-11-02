9e6cfd7dd132fa7b89391c543ab5d1e737b24a47
sdr <orbittar@wpi.edu>
2024-11-02 15:51:37 -0400

    Found a better FTT size

--- module_1/main.py
+++ module_1/main.py
@@ -33,11 +33,12 @@ sdr.rx_lo = int(carrier_freq) # Local oscillator frequency, just set it to the c
 sdr.rx_buffer_size = int(buffer_size)
 
 # Adding gain
+sdr.gain_control_mode_chan0 = "manual"
 sdr.rx_hardwaregain_chan0 = 50  # Adjust gain (dB)
 
 ## Spectogram Variables
-slice_size = 256
-overlap_size = 64
+slice_size = 1024
+overlap_size = 128
 
 #-------------------------------------------------------------------------------------------------------------
 # Creating the spectogram
@@ -47,6 +48,11 @@ overlap_size = 64
 num_samples = math.ceil(sampling_freq*target_time_duration/buffer_size) * buffer_size
 data = findingOptimalRates.recieveSamples(num_samples, buffer_size, sdr, sampling_freq)
 
+#data = np.load('pluto_samples.npy') // If you want to load samples from a file instead of recieving.
+
 ## Drawing spectogram
 plt.specgram(data, Fs = sampling_freq, NFFT=slice_size, noverlap=overlap_size, Fc=carrier_freq)
+plt.title('Spectrogram of RF on '+ str(carrier_freq/1000000.0) + "MHz band")
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
 plt.show()
