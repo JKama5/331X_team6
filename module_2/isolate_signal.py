0e7b589be9e58b134b812bd94d2c44189adeffc6
sdr <orbittar@wpi.edu>
2024-11-10 10:53:50 -0500

    Finished module 2 with deliverables :)

--- module_2/isolate_signal.py
+++ module_2/isolate_signal.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 ## Returns only the range of sample containing a signal with magnitude over threshhold_abs.
+## Default file path is module_2/pluto_samples.npy but set this to whatever file you need when calling function in main
 def isolateSignal(file = 'module_2/pluto_samples.npy', sampling_freq = 521000, threshhold_abs = 100):
 
     ## Retreave samples from file:
@@ -12,11 +13,13 @@ def isolateSignal(file = 'module_2/pluto_samples.npy', sampling_freq = 521000, t
     ## Isolating signal in time domain:
     start_index, end_index = None, None
 
+    #Move forward in the array until there is a signal.
     for i in range(len(samples) - 1):
         if abs(samples[i]) >= threshhold_abs:
             start_index = i
             break
 
+    #Move backwards in the array until there is a signal.
     for i in reversed(range(len(samples) - 1)):
         if abs(samples[i]) >= threshhold_abs:
             end_index = i
@@ -26,6 +29,7 @@ def isolateSignal(file = 'module_2/pluto_samples.npy', sampling_freq = 521000, t
         print("No signal found!")
         sys.exit(0)
 
+    # Isolate the range between between when the signal started and ended.
     signal_samples = samples[start_index:end_index]
     print("Isolated signal from", start_index/sampling_freq, "s to", end_index/sampling_freq, "s.")
     return signal_samples
--- module_2/main.py
+++ module_2/main.py
@@ -4,22 +4,24 @@ import matplotlib.pyplot as plt
 
 
 ## Defining parameters
-file = 'module_2/pluto_samples.npy'
-sampling_freq = 521000
+file = 'module_2/pluto_samples.npy' # make path to your saved 30 seconds of data 
+sampling_freq = 521000 # match this frequency to what you sampled your data in 
 T = 1 / sampling_freq
 carrier_freq = 433900000
 threshhold_abs = 100
 
 ## Isolate the blip from 30 seconds of samples:
-samples = isolate_signal.isolateSignal(file, sampling_freq, threshhold_abs)
+samples = isolate_signal.isolateSignal(file, sampling_freq, threshhold_abs) # See isolate_signal.py for brief
 
 t = np.arange(0, len(samples)*T, T) # time vector
 
-# v = np.real(samples * np.exp(1j * 2*np.pi * carrier_freq * t)) # Bandpass waveform
+Real = np.real(samples)
+Imag = np.imag(samples)
+sR = abs(samples)
+phase = np.atan2(Real,Imag)
+
 
 ## Plot amplitude over time:
-R = abs(samples)
-
 plt.figure(0)
 plt.plot(t, R)
 plt.title("Amplitude Plot of the Signal")
@@ -28,11 +30,17 @@ plt.ylabel('Amplitude')
 plt.show()
 
 ## Plot phase over time:
-Θ = np.angle(samples)
-
 plt.figure(1)
-plt.plot(t, Θ)
+plt.plot(t, phase)
 plt.title("Phase Plot of the Signal")
 plt.xlabel('Time [sec]')
 plt.ylabel('Phase [rad]')
+plt.show()
+
+## Plot IQ data:
+plt.figure(2)
+plt.scatter(Real, Imag, linewidths=0.3)
+plt.title("Signal Constellation of the Signal")
+plt.xlabel('I')
+plt.ylabel('Q')
 plt.show()
