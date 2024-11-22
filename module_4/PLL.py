fe9bb7a3b3ee7ba22ed4da84d7e6815735fb7dfe
Octavio Bittar <orbittar@wpi.edu>
2024-11-22 15:13:02 -0500

    Module 4: Shifting phases to 0

--- module_4/PLL.py
+++ module_4/PLL.py
@@ -3,9 +3,6 @@ import adi
 import time
 import math
 import numpy as np
-import find_offset
-import isolate_signal
-import module_4.receiver as receiver
 import matplotlib.pyplot as plt
 
 # -----------------------------------------------------------
@@ -17,4 +14,15 @@ import matplotlib.pyplot as plt
 # Jack Kamataris (jakamataris@wpi.edu)
 # -----------------------------------------------------------
 
+def fineFrequencyCorrection(samples):
+    R = abs(samples)
+    theta = np.arctan2(np.imag(samples), np.real(samples))
+
+    theta_shifted_Q1 = theta % np.pi/2
+    theta_shifted_0 = theta_shifted_Q1 - np.pi/4
+
+
+    samples_shifted_0 = R * np.exp(1j*theta_shifted_0)
+
+    return samples_shifted_0
 
--- module_4/main.py
+++ module_4/main.py
@@ -4,6 +4,7 @@ import time
 import math
 import numpy as np
 import receiver
+import pll
 import matplotlib.pyplot as plt
 
 # -----------------------------------------------------------
@@ -32,23 +33,23 @@ bandwidth = 1e6 # The bandwidth of the receiver.
 buffer_size = 2**21 # size of the sample buffer on the SDR (each sdr.rx() call returns this amount of samples).
 
 # FFT Variables
-slice_size = 1024
-overlap_size = 128
+slice_size = 512
+overlap_size = 64
 
 #-------------------------------------------------------------------------------------------------------------
 # Sampling/Receiving
 #-------------------------------------------------------------------------------------------------------------
 
 # Configuring Pluto SDR
-sdr.sample_rate = int(sampling_freq)
-sdr.center_freq = int(carrier_freq)
-sdr.rx_rf_bandwidth = int(bandwidth)
-sdr.rx_lo = int(carrier_freq) # Local oscillator frequency, initially set to carrier frequency.
-sdr.rx_buffer_size = int(buffer_size)
+# sdr.sample_rate = int(sampling_freq)
+# sdr.center_freq = int(carrier_freq)
+# sdr.rx_rf_bandwidth = int(bandwidth)
+# sdr.rx_lo = int(carrier_freq) # Local oscillator frequency, initially set to carrier frequency.
+# sdr.rx_buffer_size = int(buffer_size)
 
 # Adding gain
-sdr.gain_control_mode_chan0 = "manual"
-sdr.rx_hardwaregain_chan0 = 50  # Adjust gain (dB)
+# sdr.gain_control_mode_chan0 = "manual"
+# sdr.rx_hardwaregain_chan0 = 50  # Adjust gain (dB)
 
 # Receiving samples
 num_samples = math.ceil(target_num_samples/buffer_size) * buffer_size
@@ -61,7 +62,7 @@ uncorrected_samples = np.load("module_4_samples/uncorrected_samples.npy")
 # Plotting original data
 t = np.arange(0, T*len(uncorrected_samples), T) # create time vector
 
-plt.figure(0)
+plt.figure("Spectrogram")
 plt.specgram(uncorrected_samples, Fs = sampling_freq, NFFT=slice_size, noverlap=overlap_size, Fc=carrier_freq)
 plt.title('Spectrogram of RF on '+ str((carrier_freq)/1e9) + "GHz band")
 plt.ylabel('Frequency [Hz]')
@@ -75,7 +76,7 @@ phase = np.atan2(Real,Imag)
 
 
 # ## Plot amplitude over time:
-# plt.figure(2)
+# plt.figure("Amplitude Plot")
 # plt.plot(t, R)
 # plt.title("Amplitude Plot of the Signal")
 # plt.xlabel('Time [sec]')
@@ -83,7 +84,7 @@ phase = np.atan2(Real,Imag)
 # plt.show()
 
 # ## Plot phase over time:
-# plt.figure(3)
+# plt.figure("Phase Plot")
 # plt.plot(t, phase)
 # plt.title("Phase Plot of the Signal")
 # plt.xlabel('Time [sec]')
@@ -91,19 +92,30 @@ phase = np.atan2(Real,Imag)
 # plt.show()
 
 ## Plot IQ data:
-plt.figure(4)
+# plt.figure("Signal Constellation Plot")
+# plt.scatter(Real, Imag, linewidths=0.3)
+# plt.title("Signal Constellation of the Signal")
+# plt.xlabel('I')
+# plt.ylabel('Q')
+# plt.show()
+
+#-------------------------------------------------------------------------------------------------------------
+# Fine Frequency Correction (PLL)
+#-------------------------------------------------------------------------------------------------------------
+
+corrected_samples = pll.fineFrequencyCorrection(uncorrected_samples)
+
+
+Real = np.real(corrected_samples)
+Imag = np.imag(corrected_samples)
+R = abs(corrected_samples)
+phase = np.atan2(Real,Imag)
+
+
+## Plot IQ data:
+plt.figure("Signal Constellation Plot")
 plt.scatter(Real, Imag, linewidths=0.3)
 plt.title("Signal Constellation of the Signal")
 plt.xlabel('I')
 plt.ylabel('Q')
 plt.show()
-
-#-------------------------------------------------------------------------------------------------------------
-# Coarse Frequency Correction
-#-------------------------------------------------------------------------------------------------------------
-
-# Find the frequency offset
-# freq_offset = find_offset.findOffset(isolated_raw_signal, sampling_freq, carrier_freq, 1, 1024, 128) # See find_offset.py
-
-# Adjusting LO to compensate for offset
-# sdr.rx_lo = int(carrier_freq + freq_offset)
